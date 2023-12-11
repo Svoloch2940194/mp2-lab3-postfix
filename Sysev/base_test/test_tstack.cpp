e1483ec44b64415ea84e34f845a8e081c69ff6bf
Seraphim Volochaev <116020688+Svoloch2940194@users.noreply.github.com>
2023-12-11 13:03:31 +0300

    Update test_tstack.cpp

--- Sysev/base_test/test_tstack.cpp
+++ Sysev/base_test/test_tstack.cpp
@@ -55,11 +55,11 @@ TEST(TStack, check_for_return_after_deletion2)
 TEST(TStack, check3)
 {
 	TStack<int> st(5);
-	ASSERT_TRUE(st.sz() == 5);
+	ASSERT_EQ(5, st.sz());
 }
 
 TEST(TStack, check4)
 {
 	TStack<int> st;
-	ASSERT_TRUE(st.sz() == 1);
+	ASSERT_EQ(1, st.sz());
 }
